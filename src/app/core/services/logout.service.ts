06f593b8cea591babef9b2ce222e1f4e9f145ac2
Michał Piotrowski <97.piotrowski.michal@gmail.com>
2020-11-22 20:22:25 +0100

    Redirection after logout

--- src/app/core/services/logout.service.ts
+++ src/app/core/services/logout.service.ts
@@ -1,17 +1,21 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {AuthService} from '../../auth/services/auth.service';
+import {Router} from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LogoutService {
 
-  constructor(private http: HttpClient, private authService: AuthService) {
+  constructor(private http: HttpClient, private authService: AuthService, private router: Router) {
   }
 
   logout(): void {
     this.http.post('logout', {})
-      .subscribe(() => this.authService.setLoggedUser(null));
+      .subscribe(() => {
+        this.authService.setLoggedUser(null);
+        this.router.navigate(['/']);
+      });
   }
 }
