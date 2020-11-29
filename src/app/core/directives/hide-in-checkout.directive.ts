6e2aa6937a7f0e3754e055e6ec5bf5715b8da8ed
Michal Piotrowski <97.piotrowski.michal@gmail.com>
2020-11-29 22:52:42 +0100

    Fix elements hiding in checkout page

--- src/app/core/directives/hide-in-checkout.directive.ts
+++ src/app/core/directives/hide-in-checkout.directive.ts
@@ -1,12 +1,13 @@
 import {Directive, OnDestroy, OnInit, TemplateRef, ViewContainerRef} from '@angular/core';
 import {NavigationEnd, Router} from '@angular/router';
-import {Subscription} from 'rxjs';
+import {BehaviorSubject, Subscription} from 'rxjs';
 
 @Directive({
   selector: '[bsHideInCheckout]'
 })
 export class HideInCheckoutDirective implements OnInit, OnDestroy {
   private routeChangeSubscription: Subscription;
+  private routeChange: BehaviorSubject<void> = new BehaviorSubject<void>(null);
 
   constructor(
     private router: Router,
@@ -20,18 +21,25 @@ export class HideInCheckoutDirective implements OnInit, OnDestroy {
       if (!(event instanceof NavigationEnd)) {
         return;
       }
-      if (!this.router.isActive('checkout', false)) {
-        this.viewContainer.clear();
-        this.viewContainer.createEmbeddedView(this.templateRef);
-      } else {
-        this.viewContainer.clear();
-      }
+
+      this.routeChange.next();
     });
+
+    this.routeChangeSubscription = this.routeChange.subscribe(() => this.onRouteChange());
   }
 
   ngOnDestroy(): void {
     if (this.routeChangeSubscription) {
       this.routeChangeSubscription.unsubscribe();
+    }
+  }
+
+  onRouteChange(): void {
+    if (!this.router.isActive('checkout', false)) {
+      this.viewContainer.clear();
+      this.viewContainer.createEmbeddedView(this.templateRef);
+    } else {
+      this.viewContainer.clear();
     }
   }
 }
