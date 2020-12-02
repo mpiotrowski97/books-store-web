da576c65980a306a2bb34270bf955722734959c6
Michal Piotrowski <97.piotrowski.michal@gmail.com>
2020-12-02 22:02:40 +0100

    Add book to the cart

--- src/app/core/models/book.ts
+++ src/app/core/models/book.ts
@@ -1,4 +1,5 @@
 export interface Book {
+  isbn: string;
   title: string;
   description: string;
   author: string;
--- src/app/main/components/book-card/book-card.component.ts
+++ src/app/main/components/book-card/book-card.component.ts
@@ -1,5 +1,8 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Book} from '../../../core/models/book';
+import {CartService} from '../../../dashboard/services/cart.service';
+import {finalize} from 'rxjs/operators';
+import {NotificationsService} from '../../../core/services/notifications.service';
 
 @Component({
   selector: 'bs-book-card',
@@ -10,9 +13,27 @@ export class BookCardComponent implements OnInit {
   @Input()
   public book: Book;
 
-  constructor() { }
+  public isProcessing = false;
+
+  constructor(
+    private cartService: CartService,
+    private notificationsService: NotificationsService
+  ) {
+  }
 
   ngOnInit(): void {
   }
 
+  handleAddToCartClick(): void {
+    this.isProcessing = true;
+
+    this.cartService
+      .addToCart(this.book.isbn)
+      .pipe(
+        finalize(() => this.isProcessing = false)
+      )
+      .subscribe(
+        () => this.notificationsService.addSuccessNotification(`${this.book.title} has been added to the cart!`)
+      );
+  }
 }
